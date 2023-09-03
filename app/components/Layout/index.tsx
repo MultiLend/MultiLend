4af4354a4bc92fea74fd9fcac44c656c91f3295c
ahmedhamedaly <ahmedhamedaly19@gmail.com>
2023-09-03 08:25:04 +0200

    fix: remove mobile phones, not responsive

--- app/components/Layout/index.tsx
+++ app/components/Layout/index.tsx
@@ -7,10 +7,16 @@ interface Props {
 
 const Layout: FC<Props> = ({ children }) => {
   return (
-    <div>
-      <Header />
-      <div className="mx-auto max-w-6xl px-4 sm:px-6 lg:px-8">{children}</div>
-    </div>
+    <>
+      <div className="hidden md:block">
+        <Header />
+        <div className="mx-auto max-w-6xl px-4 sm:px-6 lg:px-8">{children}</div>
+      </div>
+      <div className="flex justify-center items-center text-center font-bold w-screen h-screen md:hidden">
+        We didn{"'"}t have time to make it responsive. Please try again on a
+        Laptop/PC {"<3"}
+      </div>
+    </>
   );
 };
 
